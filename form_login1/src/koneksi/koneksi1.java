8cc5fbd4979270a02f70a66634c02d3da3402146
Mulya Safitri <79971537+MulyaSafitri15@users.noreply.github.com>
2021-07-11 14:24:22 +0700

    Add files via upload

---
+++ form_login1/src/koneksi/koneksi1.java
@@ -0,0 +1,19 @@
+package koneksi;
+import com.mysql.jdbc.Driver;
+ import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/** 
+ *
+ * @author User
+ */
+public class koneksi1 {
+    private static Connection koneksi1;
+    public static Connection GetConnection()throws SQLException{
+        if (koneksi1==null){
+            Driver driver = new Driver();
+            koneksi1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/loginprojek1","root","");
+        }
+        return koneksi1;
+    }
+}
